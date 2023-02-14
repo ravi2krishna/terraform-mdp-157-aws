fbaac8174610d54fbf7a26c4ed249d4435a50952
ravi2krishna <ravi2krishna@gmail.com>
2023-02-14 15:30:11 +0000

    ecomm deployment script

---
+++ ecomm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo yum -y install git
+sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
